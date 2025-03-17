36e938de85e2ab46b85001fb6fb37a1146564464
VishalTorne221992 <vt221992@gmail.com>
2025-03-17 19:46:25 +0530

    server settings12

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 8080,
+    allowedHosts: 'https://shoppe-globe-app.onrender.com',
     proxy: {
       '/api': {
         target: 'http://localhost:4002',
